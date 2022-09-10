6bd46670d9142c140d4166c011202761d80f48a2
Lautaro fernandez <lautyfz1703@gmail.com>
2022-09-09 23:47:55 -0300

    create cart,itemslistcontainer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/NavBar";
 
-function App() {
+import "./App.css";
+import ItemListContainer from "./components/ItemsListContainer";
+
+const  App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Navbar />
+    <ItemListContainer saludo="hola usuario que catalogo desea visualizar"/>
     </div>
-  );
+    );
 }
 
 export default App;
+
+
---
+++ src/components/ItemsListContainer.js
@@ -0,0 +1,4 @@
+const ItemListContainer =({saludo})=>{
+    return <h1>{saludo}</h1>
+}
+export default ItemListContainer;
